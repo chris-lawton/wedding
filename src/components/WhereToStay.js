b34124ca0a3247fc07806c035bf9cdadaa1005df
Chris Lawton <chris.lawton@torchbox.com>
2018-12-02 21:31:30 +0000

    fixes the broken links

--- src/components/WhereToStay.js
+++ src/components/WhereToStay.js
@@ -8,8 +8,7 @@ const WhereToStay = () => (
     <div className="hotels">
       <div>
         <h3>Dinam View B&amp;B, Llandinam</h3>
-        <p><a href="https://www.facebook.com/Lisajaynewill">Facebook</a></p>
-        <p><a href="www.airbnb.co.uk/rooms/24765299">AirBnB</a></p>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://www.airbnb.co.uk/rooms/24765299">AirBnB</a></p>
       </div>
 
       <div>
@@ -21,95 +20,90 @@ const WhereToStay = () => (
       <div>
         <h3>Green Lane, Llandinam</h3>
         <p>4 bedroom holiday house less than 1 mile form Plas Dinam. Please phone <a href="tel:07702187002">07702 187002</a> to book via the owner.</p>
-        <p><a href="www.holidaycottages.co.uk/wales/mid-wales/farmhouse-retreat">Website</a></p>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://www.holidaycottages.co.uk/wales/mid-wales/farmhouse-retreat">Website</a></p>
       </div>
 
       <div>
         <h3>Slate House Lodges</h3>
         <p>2 bedroom lodges, 1 mile from Plas Dinam</p>
-        <a href="www.hoseasons.co.uk/lodges/slate-house-lodges-slat">Website</a>
-      </div>
-
-      <div>
-        <h3>Maesmawr Hall Hotel</h3>
-        <p>2 miles away</p>
-        <a href="www.maesmawr.co.uk">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.hoseasons.co.uk/lodges/slate-house-lodges-slat">Website</a>
       </div>
 
       <div>
         <h3>Trewythen Farm Lodges, Llandinam</h3>
         <p>2 miles away</p>
-        <a href="www.hoseasons.co.uk/lodges/trewythen-lodges-trew">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.hoseasons.co.uk/lodges/trewythen-lodges-trew">Website</a>
       </div>
 
       <div>
-        <h3>Gellidywyll Holiday cottages, Llandinam</h3>
+        <h3>Gellidywyll holiday barns, Llandinam</h3>
         <p>1⁄2 mile away</p>
-        <a href="www.gellidywyllcaravanpark.co.uk/Accommodation.aspx">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.gellidywyllholidaybarns.co.uk/">Website</a>
       </div>
 
       <div>
         <h3>Broneirion, Llandinam</h3>
         <p>Girl guide centre offering house hire</p>
-        <a href="www.broneirion.com">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.broneirion.com">Website</a>
       </div>
 
       <div>
         <h3>Mid Wales Art Centre - B&amp;B</h3>
         <p>1 mile away</p>
-        <a href="www.midwalesarts.org.uk/bandb.php">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.midwalesarts.org.uk/accommodation.html">Website</a>
       </div>
 
       <div>
         <h3>Trewythen Hotel</h3>
         <p>Llanidloes, SY18 6BW</p>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.trewythenllanidloes.co.uk/">Website</a>
       </div>
 
       <div>
         <h3>The Unicorn Hotel</h3>
         <p>Llanidloes, SY18 6EE</p>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.unicornllanidloes.co.uk/">The Unicorn Hotel</a>
       </div>
 
       <div>
         <h3>Luxury Lodges Wales, Trefeglwys</h3>
-        <p><a href="www.luxury-lodges-wales.com">Website</a></p>
-        <p><a href="tel:01686430491">01686 430491</a></p>
+        <p><a target="_blank" rel="noopener noreferrer" href="https://www.luxury-lodges-wales.com">Website</a></p>
       </div>
 
       <div>
-        <h3>Old Chapel Farm, Banhadlog, Llanidloes, SY18 6JR.</h3>
-        <p>Yurts and a bunk barn.</p>
-        <a href="www.oldchapelfarm.org">Website</a>
+        <h3>Old Chapel Farm</h3>
+        <p>Llanidloes, SY18 6JR. Yurts and a bunk barn.</p>
+        <a target="_blank" rel="noopener noreferrer" href="http://thewildernesstrust.org/old-chapel-farm/yurt-holidays/">Website</a>
       </div>
 
       <div>
-        <h3>AIRBNB Barn in Llawryglyn</h3>
+        <h3>AirBnB Barn, Llawryglyn</h3>
         <p>4 guests</p>
-        <a href="www.airbnb.co.uk/rooms/1129567">AirBnB</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.airbnb.co.uk/rooms/1129567">AirBnB</a>
       </div>
 
       <div>
         <h3>Highgate Accommodation</h3>
         <p>Sleeps up to 20 near Newtown</p>
-        <a href="www.highgate-accommodation.co.uk">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.highgate-accommodation.co.uk">Website</a>
       </div>
 
       <div>
         <h3>Plasnewydd Bunkhouse</h3>
         <p>Barn sleeping 27 near Llanidloes</p>
-        <a href="www.plasnewyddbunkhouse.co.uk">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.plasnewyddbunkhouse.co.uk/">Website</a>
       </div>
 
       <div>
         <h3>Glyngynwydd Cottages</h3>
         <p>Near Llanidloes, 5 self catering cottages sleeping up to 19</p>
-        <a href="www.glyngynwydd.co.uk">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.glyngynwydd.co.uk">Website</a>
       </div>
 
       <div>
         <h3>Garth Barns</h3>
         <p>Near Llanidloes, sleeps up to 36</p>
-        <a href="www.bigcountrycottage.co.uk">Website</a>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.bigcountrycottage.co.uk/">Website</a>
       </div>
 
     </div>
