cdb75e38ac4da94ddea60a9bef9f52052afd72f2
Chris Lawton <chris.lawton@torchbox.com>
2019-01-02 20:56:04 +0000

    scroll to top of page on route change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+render(<BrowserRouter><ScrollToTop><App /></ScrollToTop></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
